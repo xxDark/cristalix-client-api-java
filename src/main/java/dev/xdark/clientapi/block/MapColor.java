b8f8116cd9c3c84f55673de2ab4ad83f914237b4
xDark <19853368+xxDark@users.noreply.github.com>
2021-01-23 17:01:52 +0300

    Add side annotations

--- src/main/java/dev/xdark/clientapi/block/MapColor.java
+++ src/main/java/dev/xdark/clientapi/block/MapColor.java
@@ -1,5 +1,9 @@
 package dev.xdark.clientapi.block;
 
+import dev.xdark.clientapi.Side;
+import dev.xdark.clientapi.SidedApi;
+
+@SidedApi(Side.SERVER)
 public interface MapColor {
 
   int getColor(int type);
