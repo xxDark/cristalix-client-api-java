eb10e88755086a622ecc4c6c82d7a745c6a3e00b
xDark <19853368+xxDark@users.noreply.github.com>
2021-01-28 17:51:35 +0300

    Temp workaroundd for sprinting event

--- src/main/java/dev/xdark/clientapi/event/entity/PlayerSprint.java
+++ src/main/java/dev/xdark/clientapi/event/entity/PlayerSprint.java
@@ -3,10 +3,9 @@ package dev.xdark.clientapi.event.entity;
 import dev.xdark.clientapi.Side;
 import dev.xdark.clientapi.SidedApi;
 import dev.xdark.clientapi.entity.EntityPlayerSP;
-import dev.xdark.clientapi.event.Cancellable;
 
 @SidedApi(Side.BOTH)
-public interface PlayerSprint extends Cancellable {
+public interface PlayerSprint {
 
   String KEY = "player_sprint";
 
