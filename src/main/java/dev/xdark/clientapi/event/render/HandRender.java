5a3a51911342039bebadbf28bedc17dbf06ce51f
xDark <19853368+xxDark@users.noreply.github.com>
2021-03-29 22:07:51 +0300

    Hand render event, switch authlibs

---
+++ src/main/java/dev/xdark/clientapi/event/render/HandRender.java
@@ -0,0 +1,10 @@
+package dev.xdark.clientapi.event.render;
+
+import dev.xdark.clientapi.Side;
+import dev.xdark.clientapi.SidedApi;
+import dev.xdark.clientapi.event.Cancellable;
+
+@SidedApi(Side.BOTH)
+public interface HandRender extends Cancellable {
+
+}
