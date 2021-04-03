298f43ec97015c30aebb9a70f384b836c4164aee
xDark <19853368+xxDark@users.noreply.github.com>
2021-04-03 21:08:35 +0300

    custom sounds

--- src/main/java/dev/xdark/clientapi/sound/SoundEvent.java
+++ src/main/java/dev/xdark/clientapi/sound/SoundEvent.java
@@ -3,9 +3,14 @@ package dev.xdark.clientapi.sound;
 import dev.xdark.clientapi.Side;
 import dev.xdark.clientapi.SidedApi;
 import dev.xdark.clientapi.resource.ResourceLocation;
+import dev.xdark.clientapi.util.CompileStub;
 
 @SidedApi(Side.SERVER)
 public interface SoundEvent {
+
+    static SoundEvent of(ResourceLocation location) {
+        throw CompileStub.create();
+    }
 
     ResourceLocation getName();
 }
