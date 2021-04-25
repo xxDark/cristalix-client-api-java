9ceb992c62788359cafb971bd9e57f2f248756f1
xDark <19853368+xxDark@users.noreply.github.com>
2021-04-25 18:50:03 +0300

    add lightning related methods

--- src/main/java/dev/xdark/clientapi/entity/EntityLightningBolt.java
+++ src/main/java/dev/xdark/clientapi/entity/EntityLightningBolt.java
@@ -4,4 +4,17 @@ import dev.xdark.clientapi.Side;
 import dev.xdark.clientapi.SidedApi;
 
 @SidedApi(Side.SERVER)
-public interface EntityLightningBolt extends EntityWeatherEffect {}
+public interface EntityLightningBolt extends EntityWeatherEffect {
+
+  int getState();
+
+  void setState(int state);
+
+  long getBoltVertex();
+
+  void setBoltVertex(long boltVertex);
+
+  int getBoltLivingTime();
+
+  void setBoltLivingTime(int boltLivingTime);
+}
