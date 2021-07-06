b653255e1d5d59965df3bbf30921897248ed82cd
xDark <19853368+xxDark@users.noreply.github.com>
2021-07-07 00:39:40 +0300

    expose builder() method

--- src/main/java/dev/xdark/clientapi/item/ItemArmor.java
+++ src/main/java/dev/xdark/clientapi/item/ItemArmor.java
@@ -3,6 +3,7 @@ package dev.xdark.clientapi.item;
 import dev.xdark.clientapi.Side;
 import dev.xdark.clientapi.SidedApi;
 import dev.xdark.clientapi.inventory.EntityEquipmentSlot;
+import dev.xdark.clientapi.util.CompileStub;
 
 import java.util.UUID;
 
@@ -19,6 +20,10 @@ public interface ItemArmor extends Item {
 
   @SidedApi(Side.SERVER)
   interface Builder extends Item.Builder {
+
+    static Builder builder() {
+      throw CompileStub.create();
+    }
 
     Builder slot(EntityEquipmentSlot slot);
 
