f819fbf625cd4af38827fc29ee61fc74b35566ad
xDark <19853368+xxDark@users.noreply.github.com>
2021-07-26 22:01:17 +0300

    Don't miss a marker!

--- src/main/java/dev/xdark/clientapi/jvm/ClassDefiner.java
+++ src/main/java/dev/xdark/clientapi/jvm/ClassDefiner.java
@@ -1,5 +1,9 @@
 package dev.xdark.clientapi.jvm;
 
+import dev.xdark.clientapi.Side;
+import dev.xdark.clientapi.SidedApi;
+
+@SidedApi(Side.SERVER)
 public interface ClassDefiner {
 
   Class<?> defineClass(String name, byte[] code, int off, int len);
